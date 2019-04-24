22b38e013b7e821bd6801c1eb50f535412d36a5f
oliv3rbell <oliv3rbell@gmail.com>
2019-04-24 10:59:54 -0600

    Login/Signup screens

---
+++ lib/Setup/register.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+
+class Register extends StatefulWidget {
+  static String tag = 'Register';
+  @override
+  RegisterPage createState() => new RegisterPage();
+}
+
+class RegisterPage extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+
+    final name = TextFormField(
+      autofocus: false,
+      initialValue: 'Name',
+      decoration: InputDecoration(
+        hintText: 'Name',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      initialValue: 'Email',
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      initialValue: 'password',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    
+    
+    final signup = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {
+          //Navigator.of(context).pushNamed(HomePage.tag);
+        },
+        padding: EdgeInsets.all(12),
+        color: Colors.redAccent,
+        child: Text('Sign Up', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    final facebook = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {
+          //Navigator.of(context).pushNamed(HomePage.tag);
+        },
+        padding: EdgeInsets.all(12),
+        color: Colors.blueAccent,
+        child: Text('Register with Facebook', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    final signIn = FlatButton(
+      child: Text(
+        'Already have a BarCrawler Account? Sign In',
+        style: TextStyle(color: Colors.black54),
+      ),
+      onPressed: () {
+        Navigator.of(context).pushNamed(LoginPage.tag);
+      },
+    );
+
+    
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+       
+            SizedBox(height: 48.0),
+            name,
+            SizedBox(height: 8.0),
+            email,
+            SizedBox(height: 4.0),
+            password,
+            SizedBox(height: 4.0),
+            signup,
+            facebook,
+            SizedBox(height: 10.0),
+            signIn,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
